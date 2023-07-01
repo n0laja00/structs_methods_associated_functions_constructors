41104f5c5ec2107486b61c61d608cd5e31d73479
Janne Lammela <lammelajanne1@gmail.com>
2023-07-01 20:25:23 +0300

    xeond commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::f32::consts::PI;
+
+//the struct of Circle.
 #[derive(Debug)]
 struct Circle {
     circumference: f32,
@@ -7,13 +9,17 @@ struct Circle {
     area: f32,
 }
 
+//Implementation block
 impl Circle {
+    //Method: Using the reference of self to read the radius to get the right number.
     fn get_circumeference(&self) -> f32 {
         2.0 * PI * self.radius
     }
+    //Method: Using the reference of self to read the radius to get diameter.
     fn get_diameter(&self) -> f32 {
         self.radius * 2.0
     }
+    //Constructor: Here's a constructor to make new circles. It takes radius and uses that to construct a new circle.
     fn circle(radius: f32) -> Self {
         Self {
             circumference: 2.0 * PI * radius,
@@ -25,9 +31,12 @@ impl Circle {
 }
 
 fn main() {
-    let circle1 = Circle::circle(2.0);
+    // Making a new circle, circle1, by giving the constructor the radius of 2.0.
+    let circle1: Circle = Circle::circle(2.0);
     println!("The circle was created! We got {:#?}", circle1);
 
+    //We only need to call the get_circumeference() function that exists within the context of the circle1.
+    //If we wanted, we could also just get the same information with "circle1.circumference" and "circle1.diameter".
     println!(
         "Calculating the circumeference of our circle gives us: {}",
         circle1.get_circumeference()
